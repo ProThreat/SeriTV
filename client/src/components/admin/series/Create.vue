bb77468e637b435e74cadd44084df66cc99ea5fe
Prothread <kevin.herdershof@hotmail.com>
2019-01-10 23:18:34 +0100

    Add page in vue
    In progress

---
+++ client/src/components/admin/series/Create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  data () {
+    return {
+        
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home/Index'
+import CreateSerie from '@/components/admin/series/Create'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/admin/series/create',
+        name: 'Create series',
+        component: CreateSerie
+      },
   ]
 })
