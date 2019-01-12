6e379225813b8b6cc4f2c53dd642d2d7b8ae3a82
Gijs Abeele <gijsvdabeele@gmail.com>
2019-01-13 00:35:58 +0100

    V1.4.2 Added content to admin series index

--- client/src/components/admin/series/Index.vue
+++ client/src/components/admin/series/Index.vue
@@ -1,19 +1,42 @@
 <template>
-  <div>
-    Series index
+  <div class="container">
+    <div class="row">
+
+        <div class="col-12 col-sm-3 mt-4" v-for="movie in movies">
+          <div class="card">
+            <img class="card-img-top" :src="movie.image">
+            <div class="card-body">
+              <h5 class="card-title">{{ movie.title }}</h5>
+              <p class="card-text">{{ movie.description.substring(0,128)}}...</p>
+              <a href="#" class="btn btn-primary">More info</a>
+            </div>
+          </div>
+        </div>
+
+    </div>
   </div>
+
+
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import MovieService from '@/services/MovieService'
 
 export default {
   data () {
     return {
+      latestMovie: {},
+      movies: {}
     }
+  },
+  async mounted () {
+    this.movies = (await MovieService.index()).data
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
