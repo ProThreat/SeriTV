29ceecce473bd184f3464a803a98d522a699fde8
dwayne <contact@dwayneh.nl>
2019-01-13 15:48:37 +0100

    V - 1.6.2 Fix header Setup movies on homepage Add single content component Setup API file Refactor MovieService

--- client/src/components/home/Index.vue
+++ client/src/components/home/Index.vue
@@ -1,38 +1,40 @@
 <template>
   <b-container>
-    <b-jumbotron class="mt-5" header="Things place" lead="The place to find cool things "></b-jumbotron>
     <b-row>
-      <b-col md="6">
-        <b-card :title="movie.title" img-src="https://i.imgur.com/IzVg5ET.png" img-alt="Image" img-top>
-          <div class="card-body">
-            <h5 class="card-title">Current things things:</h5>
-          </div>
-        </b-card>
-      </b-col>
-      <b-col md="6">
-
+      <b-col
+      v-for="movie in movies"
+      :key="movie.title"
+      lg="4"
+      class="mt-2"
+      >
+        <single-content
+          :title="movie.title"
+          :shortDescription="movie.short_description"
+          :coverImage="movie.image"
+          :year="movie.year"
+        />
       </b-col>
     </b-row>
+    <b-jumbotron class="mt-5" header="Things place" lead="The place to find cool things "></b-jumbotron>
   </b-container>
 </template>
 
 <script>
 import MovieService from '@/services/MovieService'
+import singleContent from '../modules/singleContent'
 
 export default {
   data () {
     return {
-      latestMovie: {},
-      movie: {
-        title: 'Avatar'
-      }
+      movies: []
     }
   },
   async mounted () {
     this.movies = (await MovieService.index()).data
   },
   components: {
-    MovieService
+    MovieService,
+    singleContent
   }
 }
 </script>
---
+++ client/src/components/modules/singleContent.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-card :title="title"
+          :img-src="coverImage"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-2"
+    >
+    <p class="card-text">
+      <span>{{ shortDescription }}</span>
+      <small>Year: {{ year }}</small>
+    </p>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'title',
+    'coverImage',
+    'shortDescription',
+    'year'
+  ]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
